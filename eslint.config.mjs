b99b17e1b6e58878c10aa02f673fe07ee7e17c2d
Nicolás Pujia <nicopujia.dev@gmail.com>
2025-03-07 15:36:53 -0300

    disable no-unused vars

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -15,6 +15,7 @@ const eslintConfig = [
     rules: {
       "@next/next/no-html-link-for-pages": "off",
       "@typescript-eslint/no-explicit-any": "off",
+      "@typescript-eslint/no-unused-vars": "off",
     },
   }),
 ];
